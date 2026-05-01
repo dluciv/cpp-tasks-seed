ff7e2ce6ab61fe814c0ceb2f4162afcac6745882
didkovskiy-v <st142215@student.spbu.ru>
2026-05-01 14:30:25 +0300

    Implement bubble_sort and quick_sort with tests and deque

--- 01.InplaceSorting/sort_demo.cpp
+++ 01.InplaceSorting/sort_demo.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <random>
 #include <iomanip>
+#include <deque>
 
 #include "collvalue.h"
 #include "sorting.h"
@@ -22,9 +23,38 @@ void shuffle_data(std::vector<CollInt>& data)
     std::shuffle(data.begin(), data.end(), std::mt19937{std::random_device{}()});
 }
 
+void add_to_deque(std::deque<std::vector<int>>& deq, const std::vector<CollInt>& vec)
+{
+    std::vector<int> int_vec;
+    int_vec.reserve(vec.size());
+    for (const auto& val : vec) {
+        int_vec.push_back(val.value);
+    }
+    deq.push_back(int_vec);
+}
+
+void print_deque(const std::deque<std::vector<int>>& deq)
+{
+    std::cout << "\n=== Deque contents ===\n";
+    int idx = 1;
+    for (const auto& vec : deq) {
+        std::cout << "Vector " << idx++ << " (size " << vec.size() << "): ";
+        if (vec.size() <= 20) {
+            for (int x : vec) std::cout << x << " ";
+        } else {
+            for (int i = 0; i < 10; ++i) std::cout << vec[i] << " ";
+            std::cout << "... ";
+            for (size_t i = vec.size() - 10; i < vec.size(); ++i) std::cout << vec[i] << " ";
+        }
+        std::cout << "\n";
+    }
+    std::cout << "=====================\n";
+}
+
 int main()
 {
     std::vector<size_t> sizes = {100, 500, 1000, 2000};
+    std::deque<std::vector<int>> results_deque;
 
     std::cout << "N\tAlgo\t\tComps\t\tSwaps\t\tMoves\n";
     std::cout << "----------------------------------------------------------------\n";
@@ -49,10 +79,14 @@ int main()
         shuffle_data(number_data);
         CollInt::reset_stats();
         quick_sort(number_data.begin(), number_data.end());
-        std::cout << n << "\tInsertion\t" << CollInt::comps << "\t\t" << CollInt::swaps << "\t\t" << CollInt::moves << "\n";
+        std::cout << n << "\tQuick\t\t" << CollInt::comps << "\t\t" << CollInt::swaps << "\t\t" << CollInt::moves << "\n";
 
+        add_to_deque(results_deque, number_data);
+        
         std::cout << "--------------------------------------------------------------\n";
     }
+    
+    print_deque(results_deque);
 
     return 0;
 }
--- 01.InplaceSorting/sorting.h
+++ 01.InplaceSorting/sorting.h
@@ -1,21 +1,78 @@
 #ifndef SORTING_H
 #define SORTING_H
 
-// Header-only
-
-// Для swap
+#include <iterator>
+#include <utility>
 #include "collvalue.h"
 
-// ----------------------------- Пузырёк (например) --------------------------------------
 template <typename Iterator>
 void bubble_sort(Iterator begin, Iterator end)
 {
+  if (begin == end) return;
+    
+    bool swapped;
+    Iterator last = end;
+    --last;
+    
+    do {
+        swapped = false;
+        for (Iterator it = begin; it != last; ++it) {
+            Iterator next = it;
+            ++next;
+            if (*next < *it) {
+                using std::swap;
+                swap(*it, *next);
+                swapped = true;
+            }
+        }
+        --last;
+    } while (swapped);
 }
 
-// --------------------------- QuickSort (например) ---------------------------------------
+template <typename Iterator>
+Iterator partition(Iterator begin, Iterator end)
+{
+    auto pivot = end;
+    --pivot;
+    
+    Iterator i = begin;
+    --i;
+    
+    for (Iterator j = begin; j != pivot; ++j) {
+        if (*j < *pivot) {
+            ++i;
+            using std::swap;
+            swap(*i, *j);
+        }
+    }
+    ++i;
+    using std::swap;
+    swap(*i, *pivot);
+    return i;
+}
+
+template <typename Iterator>
+void quick_sort_helper(Iterator begin, Iterator end)
+{
+    if (begin == end) return;
+    
+    Iterator next = begin;
+    ++next;
+    if (next == end) return;
+    
+    Iterator pivot_pos = partition(begin, end);
+    
+    quick_sort_helper(begin, pivot_pos);
+    
+    Iterator pivot_next = pivot_pos;
+    ++pivot_next;
+    quick_sort_helper(pivot_next, end);
+}
+
 template <typename Iterator>
 void quick_sort(Iterator begin, Iterator end)
 {
+  quick_sort_helper(begin, end);
 }
 
 #endif // SORTING_H
