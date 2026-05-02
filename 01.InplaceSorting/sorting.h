8a2b187baaab1bf33916aa9645dd3b49b4a11575
Alexander <st140044@student.spbu.ru>
2026-05-02 03:20:50 +0300

    Adding sorting algorhitms

--- 01.InplaceSorting/sorting.h
+++ 01.InplaceSorting/sorting.h
@@ -10,12 +10,40 @@
 template <typename Iterator>
 void bubble_sort(Iterator begin, Iterator end)
 {
+    bool swapped = true;
+    while (swapped) {
+        swapped = false;
+        for (Iterator it = begin; std::next(it) != end; ++it) {
+            if (*std::next(it) < *it) {
+                std::iter_swap(it, std::next(it));
+                swapped = true;
+            }
+        }
+    }
 }
 
 // --------------------------- QuickSort (например) ---------------------------------------
 template <typename Iterator>
 void quick_sort(Iterator begin, Iterator end)
 {
+    // чтобы не возникло риска бесконечной рекурсии
+    if (begin == end || std::next(begin) == end) {
+        return;
+    }
+
+    auto pivot = *std::prev(end);
+    auto i = begin;
+    for (auto j = begin; j != std::prev(end); ++j) {
+        if (*j < pivot) {
+            std::iter_swap(i, j);
+            ++i;
+        }
+    }
+    std::iter_swap(i, std::prev(end));
+    Iterator pivot_it = i;
+
+    quick_sort(begin, pivot_it);
+    quick_sort(std::next(pivot_it), end);
 }
 
 #endif // SORTING_H
