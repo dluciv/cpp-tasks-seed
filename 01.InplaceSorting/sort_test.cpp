04fa5303f10974f1c5b1c23a83cb160a62cacd4f
vera <svera3769@gmail.com>
2026-05-05 02:49:06 +0300

    add hw1

--- 01.InplaceSorting/sort_test.cpp
+++ 01.InplaceSorting/sort_test.cpp
@@ -1,11 +1,66 @@
 #include <gtest/gtest.h>
-
-// А вот тут collvalue.h не надо, берите просто инты
+#include <vector>
+#include <algorithm>
 #include "sorting.h"
 
-// Но проверьте, что сортировки таки работают...
+// тест для bubble_sort
+TEST(BubbleSortTest, EmptyArray) {
+    std::vector<int> v;
+    bubble_sort(v.begin(), v.end());
+    EXPECT_TRUE(v.empty());
+}
 
-TEST(FooSuite, FooTest)
-{
-    EXPECT_EQ(0, 0);
+TEST(BubbleSortTest, SingleElement) {
+    std::vector<int> v = {42};
+    bubble_sort(v.begin(), v.end());
+    EXPECT_EQ(v[0], 42);
+}
+
+TEST(BubbleSortTest, Sorted) {
+    std::vector<int> v = {1,2,3,4,5};
+    bubble_sort(v.begin(), v.end());
+    EXPECT_TRUE(std::is_sorted(v.begin(), v.end()));
+}
+
+TEST(BubbleSortTest, ReverseSorted) {
+    std::vector<int> v = {5,4,3,2,1};
+    bubble_sort(v.begin(), v.end());
+    EXPECT_TRUE(std::is_sorted(v.begin(), v.end()));
+}
+
+TEST(BubbleSortTest, Random) {
+    std::vector<int> v = {3,1,4,1,5,9,2,6};
+    bubble_sort(v.begin(), v.end());
+    EXPECT_TRUE(std::is_sorted(v.begin(), v.end()));
+}
+
+// тесты для quick_sort
+TEST(QuickSortTest, EmptyArray) {
+    std::vector<int> v;
+    quick_sort(v.begin(), v.end());
+    EXPECT_TRUE(v.empty());
+}
+
+TEST(QuickSortTest, SingleElement) {
+    std::vector<int> v = {42};
+    quick_sort(v.begin(), v.end());
+    EXPECT_EQ(v[0], 42);
+}
+
+TEST(QuickSortTest, Sorted) {
+    std::vector<int> v = {1,2,3,4,5};
+    quick_sort(v.begin(), v.end());
+    EXPECT_TRUE(std::is_sorted(v.begin(), v.end()));
+}
+
+TEST(QuickSortTest, ReverseSorted) {
+    std::vector<int> v = {5,4,3,2,1};
+    quick_sort(v.begin(), v.end());
+    EXPECT_TRUE(std::is_sorted(v.begin(), v.end()));
+}
+
+TEST(QuickSortTest, Random) {
+    std::vector<int> v = {3,1,4,1,5,9,2,6};
+    quick_sort(v.begin(), v.end());
+    EXPECT_TRUE(std::is_sorted(v.begin(), v.end()));
 }
