eebc94a240bc4fec533a0c1f99ddf120f000648d
Alex-Greeny <alex_greeny17@gmail.com>
2026-05-04 14:26:03 +0300

    sorts with tests

--- 01.InplaceSorting/sort_test.cpp
+++ 01.InplaceSorting/sort_test.cpp
@@ -1,11 +1,40 @@
 #include <gtest/gtest.h>
-
-// А вот тут collvalue.h не надо, берите просто инты
+#include <vector>
+#include <algorithm>
 #include "sorting.h"
 
-// Но проверьте, что сортировки таки работают...
-
-TEST(FooSuite, FooTest)
+// Тестируем Bubble Sort
+TEST(SortSuite, BubbleSortTest)
 {
-    EXPECT_EQ(0, 0);
+    std::vector<int> arr = {5, 2, 9, 1, 5, 6};
+    std::vector<int> expected = {1, 2, 5, 5, 6, 9};
+
+    bubble_sort(arr.begin(), arr.end());
+
+    EXPECT_EQ(arr, expected);
+}
+
+TEST(SortSuite, BubbleSortEmptyTest)
+{
+    std::vector<int> arr = {};
+    bubble_sort(arr.begin(), arr.end());
+    EXPECT_TRUE(arr.empty());
+}
+
+// Тестируем Quick Sort
+TEST(SortSuite, QuickSortTest)
+{
+    std::vector<int> arr = {10, -3, 21, 0, 0, 5};
+    std::vector<int> expected = {-3, 0, 0, 5, 10, 21};
+
+    quick_sort(arr.begin(), arr.end());
+
+    EXPECT_EQ(arr, expected);
+}
+
+TEST(SortSuite, QuickSortSortedTest)
+{
+    std::vector<int> arr = {1, 2, 3, 4, 5};
+    quick_sort(arr.begin(), arr.end());
+    EXPECT_TRUE(std::is_sorted(arr.begin(), arr.end()));
 }
